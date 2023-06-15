73e347ef304debb912b1689f84f4d14ea84c6d3c
Temkin99 <akbertemkin@gmail.com>
2023-06-15 14:45:34 -0700

    5-printf.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A code that print line of function
+ * Return: 0
+ */
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+}
