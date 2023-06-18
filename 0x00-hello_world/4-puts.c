e053bcae502e42e25dc5a7e2a01ea8daef40b705
Temkin99 <akbertemkin@gmail.com>
2023-06-18 00:23:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,24 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
- * main - A code that print line of function
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
