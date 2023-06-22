ddb4b15c64cd1e442c8448f994870a3a1039a5b5
Temkin99 <akbertemkin@gmail.com>
2023-06-22 04:52:13 -0700

    main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _is lower(int c);
+int _is alpha(int c);
+int print_sign(int n);
+int _abr(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void time_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+#endif
