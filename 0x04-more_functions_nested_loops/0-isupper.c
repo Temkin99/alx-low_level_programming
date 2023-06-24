d56e99399701ebf53dda9dfc0329f274410dc4d8
Temkin99 <akbertemkin@gmail.com>
2023-06-23 23:59:29 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,16 +4,23 @@
  * @c: char to check
  * return 0 or 1
  */
-int _isupper(int c)
-{
-int i;
+bool is_uppercase(char c) {
+    return c >= 'A' && c <= 'Z';
+}
 
-    for (i = 'A'; i <= 'Z'; i++)
- {
-    if (i == c)
- {
-        return 1;  
-    } 
+bool match_character(char c1, char c2) {
+    return c1 == c2;
+}
+
+int _isupper(int c) {
+    const char uppercase_letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    const int num_uppercase_letters = sizeof(uppercase_letters) / sizeof(uppercase_letters[0]);
+
+    for (int i = 0; i < num_uppercase_letters; i++) {
+        if (match_character(c, uppercase_letters[i])) {
+            return 1;
+        }
     }
-        return 0;   
+
+    return 0;
 }
