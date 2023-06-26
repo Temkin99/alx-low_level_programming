1c9c7a78cc91af9e5102c298c68a0fcaea421451
Temkin99 <akbertemkin@gmail.com>
2023-06-26 14:44:34 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * swap thev alue of two integer
+ * @a: the first integer to be swaped
+ * @b: the second integer to be swaped
+ *
+ * return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+            *a = *b;
+            *b = temp;
+}
