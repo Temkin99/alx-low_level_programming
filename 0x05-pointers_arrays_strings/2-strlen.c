e19edf01a99d2feebd67c5eab5c3840809556cf3
Temkin99 <akbertemkin@gmail.com>
2023-06-26 15:38:47 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,9 +5,11 @@
  * return: the length of @str
  */
 
-int _strlen(char* s) {
+int _strlen(char* s) 
+{
     int len = 0;
-    while (*s != '\0') {
+    while (*s != '\0') 
+    {
         len++;
         s++;
     }
