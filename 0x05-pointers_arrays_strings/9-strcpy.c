94dbec6c6559e9615f4da792998573bf4d47f1d2
Temkin99 <akbertemkin@gmail.com>
2023-07-03 12:14:42 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcpy - main function
+ *
+ * @dest: pointer of function
+ *
+ * @src: ointer of function
+ *
+ * Return: returnes char
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
