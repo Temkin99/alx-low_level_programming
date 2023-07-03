13b077bfc071eb7a6c9c1370f59332975fd8778d
Temkin99 <akbertemkin@gmail.com>
2023-07-03 06:53:38 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_memset - file memory with constant byte
+ * @s: memory area to filled
+ * @b: char to copy
+ * @n: number of time to be copy b
+ * return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i = 0;
+while (i < n)
+{
+dest[i] = src [i];
+i++;
+}
+return (dest);
+}
+
