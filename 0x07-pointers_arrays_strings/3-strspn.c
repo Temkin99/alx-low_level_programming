3627c4f978cb680444317cfe143383d74e8312fc
Temkin99 <akbertemkin@gmail.com>
2023-07-03 11:15:09 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - main func
+ * @s: char pointer var
+ * @accept: char pointer var
+ * Return: returns ui
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0' && s[i] != accept[j])
+			j++;
+		if (accept[j] == '\0')
+			return (i);
+		i++;
+	}
+	return (i);
+}
