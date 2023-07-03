9ef27ebe2e98f0821c68d2feed2754a39a8ab053
Temkin99 <akbertemkin@gmail.com>
2023-07-03 10:55:11 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,12 +8,22 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-int i = 0;
-for (; n > 0; i++)
-{
-s[i] = b;
-n--;
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
 }
-return (s);
-}
+
+
+
+
+
+
+
+
+
 
