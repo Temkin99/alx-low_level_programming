808871cb544503a641a5d2a1544e5dfb1646baa6
Temkin99 <akbertemkin@gmail.com>
2023-07-07 12:11:49 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: numof arg
+ * @argv: value of arg
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", --argc);
+	(void) argv;
+	return (0);
+}
