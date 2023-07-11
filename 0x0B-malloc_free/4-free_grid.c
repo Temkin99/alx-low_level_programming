528128a962c16cd5d68c93535b63e35b6a8de286
Temkin99 <akbertemkin@gmail.com>
2023-07-11 04:57:42 -0700

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#includ "main.h"
+
+/**
+ * free_grid - main func
+ * @grid: int pointer of a pointer
+ * @height: int var
+ * Return: nothing
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
