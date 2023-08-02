bedff172d0ce6074236374d781cfd4507fe13e93
Temkin99 <akbertemkin@gmail.com>
2023-08-02 01:10:06 -0700

    update

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * free_listint2 - free a linked list of int and add null
+ * @head: pointer of pointer that contain adress of
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	if (head != NULL)
+	{
+		listint_t *tmp, *store = *head;
+
+		while (store != NULL)
+		{
+			tmp = store;
+			store = store->next;
+			free(tmp);
+		}
+		*head = NULL;
+	}
+	else
+		return;
+}
